51da09d574d5d2112fac7d63ff49529a3cc75da2
Martin-Luther ETOUMAN <m.etouman@rhinostone.com>
2014-01-07 16:53:43 +0100

    added start all route and stop all route

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -40,6 +40,82 @@ ShutdownController = {
             );
         }
         this.renderJSON({status:"ok"});
+    },
+
+    /**
+     * Start bundle
+     *
+     * @param {object} options - JSON Options
+     * */
+    startBundle : function(req, res){
+
+        var _this = this;
+
+        try {
+            var bundle  = req.param("bundle") ;
+            var task    = req.param("task");
+            var env     = req.param("env") || 'prod';
+            //var token = appConfig.shutdownService.token;
+
+            taskHandler = getContext("taskHandler");
+            console.log("starting bundle ", bundle);
+             taskHandler.process({
+                task    : task,
+                pid     : null,
+                bundle  : bundle,
+                env     : env
+            });
+
+
+        } catch (err) {
+            logger.error(
+                'shutdown',
+                'SHUTDOWN:CONTROLLER:ERR:2',
+                err,
+                err.stack
+            );
+        }
+        this.renderJSON({status:"ok"});
+    },
+    startAllBundles : function(req, res){
+        var _this = this;
+        var env     = req.param("env") || 'prod';
+        var config  = this.getConfig();
+        var conf = {
+            'bundlesPath'   : config.bundlesPath,
+            'bundles'       : process.getMountedBundlesSync(),
+            'env'           : env
+        };
+
+        taskHandler = getContext("taskHandler");
+        taskHandler.startAllBundles(conf, function onStarted(err){
+            if(err)
+                console.error(err)
+            else
+                console.log("All bundles started with success");
+
+            _this.renderJSON({status:"ok"});
+        });
+    },
+    stopAllBundles : function(req, res){
+
+        var _this = this;
+        var runnunigBundles = process.getRunningBundlesSync();
+
+        if (runnunigBundles.count() > 0) {
+            taskHandler = getContext("taskHandler");
+            taskHandler.stopAllBundles(runnunigBundles, 'SIGINT', function onStarted(err){
+                if(err)
+                    console.error(err)
+                else
+                    console.log("All bundles started with success");
+
+                _this.renderJSON({status:"ok"});
+            });
+        } else {
+            _this.renderJSON({status:"ok"});
+        }
+
     }
 };
 
